b34b4fd88a92cab25bbf7f849187fbb153bf4463
Ubuntu <ubuntu@ip-172-31-41-55.ap-northeast-2.compute.internal>
2021-10-05 12:50:23 +0000

    test

--- include/threads/vaddr.h
+++ include/threads/vaddr.h
@@ -12,13 +12,13 @@
  * See pte.h for functions and macros specifically for x86
  * hardware page tables. */
 
-#define BITMASK(SHIFT, CNT) (((1ul << (CNT)) - 1) << (SHIFT))
+#define BITMASK(SHIFT, CNT) (((1ul << (CNT)) - 1) << (SHIFT))  // 1 unsigned long                                   
 
 /* Page offset (bits 0:12). */
 #define PGSHIFT 0                          /* Index of first offset bit. */
 #define PGBITS  12                         /* Number of offset bits. */
 #define PGSIZE  (1 << PGBITS)              /* Bytes in a page. */
-#define PGMASK  BITMASK(PGSHIFT, PGBITS)   /* Page offset bits (0:12). */
+#define PGMASK  BITMASK(PGSHIFT, PGBITS)   /* Page offset bits (0:12). */    // 1111 1111 1111 인듯?
 
 /* Offset within a page. */
 #define pg_ofs(va) ((uint64_t) (va) & PGMASK)
@@ -41,7 +41,7 @@
 #define is_user_vaddr(vaddr) (!is_kernel_vaddr((vaddr)))
 
 /* Returns true if VADDR is a kernel virtual address. */
-#define is_kernel_vaddr(vaddr) ((uint64_t)(vaddr) >= KERN_BASE)
+#define is_kernel_vaddr(vaddr) ((uint64_t)(vaddr) >= KERN_BASE)           // 커널의 VADDR은 KERN_BASE 이후인가?? 그리고 남은 부분까지 차지 하는건지??
 
 // FIXME: add checking
 /* Returns kernel virtual address at which physical address PADDR
